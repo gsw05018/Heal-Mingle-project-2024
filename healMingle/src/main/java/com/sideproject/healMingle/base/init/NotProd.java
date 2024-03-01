7a471c26fabdac1cbc981c4bdeac5028298b96dd
한인철 <gsw05018@naver.com>
2024-03-01 16:50:26 +0900

    feat : 기본 회원 생성'

---
+++ healMingle/src/main/java/com/sideproject/healMingle/base/init/NotProd.java
@@ -0,0 +1,24 @@
+package com.sideproject.healMingle.base.init;
+
+import com.sideproject.healMingle.boundContext.member.service.MemberService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
+
+@Configuration
+@Profile ( "!prod" )
+public class NotProd {
+	@Bean
+	public ApplicationRunner init ( MemberService memberService ){
+		return args -> {
+			memberService.join ( "admin", "1234","admin","admin@test.com", null );
+
+			IntStream.rangeClosed ( 1, 3 ).forEach ( i -> {
+				memberService.join ( "user"  + i, "1234", "user" + i, "user" + "@test.com", null);
+			} );
+		};
+	}
+}
